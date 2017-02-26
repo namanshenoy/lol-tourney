29f538523ed51e33c34cb26a07aa1ed38fe3ef7d
namanshenoy <naman.skillet@gmail.com>
2017-02-26 03:21:31 -0500

    Fixed teams in tournament model Added response to form

--- webapp/views.py
+++ webapp/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render  
+from django.shortcuts import render
 from forms import *
 
+
 # Create your views here.
+
+
 def index(request):
-  if request.POST:
-    form = TournamentForm(request.POST)
-    if form.is_valid():
-      form.save()
-    return render(request, 'index.html', {'form':form,})
-  else:
-    form = TournamentForm()
-    return render(request, 'index.html', {'form':form,})
+    context = {}
+    if request.POST:
+        form = TournamentForm(request.POST)
+
+        if form.is_valid():
+            saved = form.save()
+            context['team_name'] = saved.title
+            context['form'] = form
+            context['registered'] = True
+        return render(request, 'index.html', context=context)
+    else:
+        form = TournamentForm()
+        return render(request, 'index.html', {'form': form, })
