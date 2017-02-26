087ae372146a311cf72f128485349761b322fc69
namanshenoy <naman.skillet@gmail.com>
2017-02-26 01:44:54 -0500

    Added some models

--- webapp/models.py
+++ webapp/models.py
@@ -1,5 +1,27 @@
 from __future__ import unicode_literals
+from datetime import datetime
+from django.contrib.auth.models import User
 
 from django.db import models
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User)
+    lol_summoner_name = models.CharField(max_length=128, null=True, blank=True)
+    date_joined = models.DateTimeField(datetime.now())
+    username = models.CharField(max_length=128, default=lol_summoner_name)
+    lol_mmr = models.IntegerField(null=True, blank=True)
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=128, null=True, blank=True)
+    players = models.ManyToManyField(UserProfile)
+    team_wins = models.IntegerField(null=True, blank=True)
+    team_losses = models.IntegerField(null=True, blank=True)
+
+
+class Tournament(models.Model):
+    title = models.CharField(max_length=128, null=True, blank=True)
+    teams = models.ForeignKey(Team, verbose_name='Teams')
