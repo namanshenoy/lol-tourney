61151414f362877ead97cb0c8a8b80815e42a1fe
namanshenoy <naman.skillet@gmail.com>
2017-03-10 01:57:43 -0500

    Refresh

--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect, reverse
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
-from forms import *
 from lxml import html
 import requests
 import pytz
@@ -25,54 +24,3 @@ def get_mmr(user_name):
 			return -2
 	else:
 		return -1
-
-
-def index(request):
-	context = {}
-	if request.POST:
-		form = TournamentForm(request.POST)
-		if form.is_valid():
-			saved = form.save()
-			context['team_name'] = saved.title
-			context['form'] = form
-			context['registered'] = True
-		return render(request, 'index.html', context=context)
-	else:
-		if request.user.is_authenticated():
-			try:
-				context['user_profile'] = UserProfile.objects.get(user=request.user)
-			except Exception, e:
-				print str(e)
-			finally:
-				context['user_profile'] = None
-
-		form = TournamentForm()
-		return render(request, 'index.html', {'form': form, })
-
-
-@login_required
-def settings(request):
-	context = {}
-	if request.POST:
-		form = UserProfileForm(request.POST)
-		if form.is_valid():
-			saved = form.save(commit=False)
-			user_profile, found = UserProfile.objects.get_or_create(user=request.user)
-			user_profile.user = request.user
-			user_profile.lol_summoner_name = saved.lol_summoner_name
-			mmr = get_mmr(saved.lol_summoner_name)
-			print mmr
-			if mmr < 0:
-				context['form'] = form
-				context['summoner_not_found'] = True
-				return render(request, 'settings.html', context=context)
-			user_profile.lol_mmr = mmr
-			user_profile.date_joined = pytz.utc.localize(datetime.now())
-			user_profile.save()
-			return HttpResponseRedirect('/')
-	if UserProfile.objects.filter(user=request.user).exists():
-		form = UserProfileForm(instance=UserProfile.objects.get(user=request.user))
-	else:
-		form = UserProfileForm()
-	context['form'] = form
-	return render(request, 'settings.html', context=context)
